07c7c6fbf3aafcff32dd1ee8099ea7d72abdc123
Rafael Costa <rafael@switchpayments.com>
2021-12-11 12:00:56 +0000

    Another batch of great changes, let me know if there is any doubt still. I left important comments with "RAAMCOSTA", check those

--- app/src/main/java/sp/bvantur/nestednavigation/ui/screens/ProfileScreen.kt
+++ app/src/main/java/sp/bvantur/nestednavigation/ui/screens/ProfileScreen.kt
@@ -9,19 +9,26 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.ramcosta.composedestinations.ExternalDetailsScreenDestination
 import com.ramcosta.composedestinations.InternalDetailsScreen2Destination
 import com.ramcosta.composedestinations.InternalDetailsScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.navigateTo
 
 @Destination
 @Composable
 fun ProfileScreen(
-	parentNavigator: DestinationsNavigator,
+	// RAAMCOSTA: since this destination is the start destination of the NavHost
+	// it cannot have mandatory navigation arguments. So if there is a navigation argument
+	// it must have a default value or be nullable
+	argument: String = "default value",
+	parentNavigator: NavHostController,
 	destinationsNavigator: DestinationsNavigator
 ) {
 	Box(
@@ -31,7 +38,7 @@ fun ProfileScreen(
 	) {
 		Column {
 			Button(onClick = {
-				parentNavigator.navigate(ExternalDetailsScreenDestination)
+				parentNavigator.navigateTo(ExternalDetailsScreenDestination)
 			}) {
 				Text(text = "External details screen")
 			}
@@ -48,5 +55,9 @@ fun ProfileScreen(
 				Text(text = "Inside details screen2")
 			}
 		}
+		Text(
+			text = "argument = $argument",
+			modifier = Modifier.align(Alignment.Center)
+		)
 	}
 }
